13b0a1110363457be33f323a246115d2d569f971
NetherMC <thelio.trinite@outlook.com>
2021-08-28 17:28:17 +0200

    Folder re-organisation

--- src/main/java/fr/nethermc/socialcommands/commands/SocialSetCommand.java
+++ src/main/java/fr/nethermc/socialcommands/commands/SocialSetCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class SocialSetCommand implements CommandExecutor {
 
@@ -20,20 +21,29 @@ public class SocialSetCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             File configFile = new File(main.getDataFolder(), "config.yml");
             YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-            SocialRegexs socialRegexs = new SocialRegexs();
+            List<String> platforms = (new SocialRegexs()).platforms;
 
-            System.out.println(socialRegexs.regexMatch(socialRegexs.discord, args[1]));
+            String platform = args[0];
+            String link = args[1];
 
-            config.set(args[0], args[1]);
+            if (!platforms.contains(platform)) {
+                player.sendMessage(
+                    "§cThe platform \"" + platform + "\" does not exist. §rAvailable platforms in the plugin: " +
+                    "https://bit.ly/3gIkrjk"
+                );
+                return true;
+            }
+
+            config.set(platform, link);
 
             try {
                 config.save(configFile);
-                player.sendMessage("§aSuccessfully saved the config for \"" + args[0] + "\"!§r");
+                player.sendMessage("§aSuccessfully saved the config for \"" + platform + "\"!§r");
                 return true;
             } catch (IOException e) {
                 player.sendMessage("§cCannot save the config. Check the console for more details.§r");
