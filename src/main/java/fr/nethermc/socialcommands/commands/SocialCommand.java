79f811468d199f39ee556c395c6ac461ec0bc5eb
NetherMC <thelio.trinite@outlook.com>
2021-08-30 13:33:01 +0200

    Added errors codes

--- src/main/java/fr/nethermc/socialcommands/commands/SocialCommand.java
+++ src/main/java/fr/nethermc/socialcommands/commands/SocialCommand.java
@@ -1,12 +1,13 @@
 package fr.nethermc.socialcommands.commands;
 
 import fr.nethermc.socialcommands.Main;
-import fr.nethermc.socialcommands.SocialRegexs;
+import fr.nethermc.socialcommands.regexs.Regexs;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.List;
@@ -22,22 +23,25 @@ public class SocialCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         if (sender instanceof Player) {
+            System.out.println(command.tabComplete(sender, alias, args));
+
             Player player = (Player) sender;
             File configFile = new File(main.getDataFolder(), "config.yml");
             YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-            List<String> platforms = SocialRegexs.PLATFORMS;
+            List<String> platforms = Regexs.PLATFORMS;
 
             if (config.get("links." + platform) != null) {
-                player.sendMessage("§9Here the " + platform + " of this server: " + config.get(platform));
+                player.sendMessage("§9Here the " + platform + " of this server: " + config.get("links." + platform));
             } else if (!platforms.contains(platform)) {
-                player.sendMessage("§cThe platform \"" + platform + "\" does not exist.");
+                player.sendMessage("§cThe platform \"" + platform + "\" doesn't exist. Error code: PLATFORM_DOESNT_EXIST");
             } else {
                 String message = "§cThe platform \"" + platform + "\" is not defined.";
                 if (player.hasPermission("socialcommands.socialset.use")) {
-                    message += " Please define it with \"/socialset " + platform + " <link>\".";
+                    message += " Define it with \"/socialset " + platform + " <link>\".";
                 }
+                message += " Error code: PLATFORM_NOT_DEFINED";
 
                 player.sendMessage(message);
             }
