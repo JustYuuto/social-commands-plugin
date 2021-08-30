8fc834021f45bf3c72032b4aaf547bb1b8051509
NetherMC <thelio.trinite@outlook.com>
2021-08-30 13:33:30 +0200

    Tab completion

--- src/main/java/fr/nethermc/socialcommands/Main.java
+++ src/main/java/fr/nethermc/socialcommands/Main.java
@@ -2,6 +2,7 @@ package fr.nethermc.socialcommands;
 
 import fr.nethermc.socialcommands.commands.SocialCommand;
 import fr.nethermc.socialcommands.commands.SocialSetCommand;
+import fr.nethermc.socialcommands.regexs.Regexs;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
@@ -10,13 +11,14 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        List<String> platforms = SocialRegexs.PLATFORMS;
+        List<String> platforms = Regexs.PLATFORMS;
 
         for (String platform : platforms) {
             getCommand(platform).setExecutor(new SocialCommand(this, platform));
         }
 
         getCommand("socialset").setExecutor(new SocialSetCommand(this));
+        getCommand("socialset").setTabCompleter(new SocialSetCommand(this));
     }
 
 }
