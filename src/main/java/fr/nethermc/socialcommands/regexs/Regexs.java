5f803bdaa12c837f8f555fce76910d38f8b61d44
Mister_KoRo <korobots.commandes@gmail.com>
2021-09-29 19:21:19 +0200

    🍱 Improve regex
    Sometimes, Discord links starts with "https://discord.com/invite/(code)". So, I improved regex.
    I'm not familiar with java regex so... it will maybe need to be fixed.

--- src/main/java/fr/nethermc/socialcommands/regexs/Regexs.java
+++ src/main/java/fr/nethermc/socialcommands/regexs/Regexs.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Regexs {
 
-    public static final String DISCORD_REGEX = "(discord\\.(gg|io)\\/)?([a-zA-Z0-9]+)";
+    public static final String DISCORD_REGEX = "(discord\\.(gg|io|com\/invite\/)\\/)?([a-zA-Z0-9]+)";
     public static final String TWITTER_REGEX = "(\\@)?([a-zA-Z0-9_]+)";
     public static final String INSTAGRAM_REGEX = "(\\@)?([a-zA-Z0-9_]+)";
     public static final String WEBSITE_REGEX = "(https?\\:(\\/\\/)?)?(([a-z\\.]+)\\.)?(([a-z]+)\\.([a-z]+))(\\/([a-zA-Z0-9_\\-\\.]+)?)?";
