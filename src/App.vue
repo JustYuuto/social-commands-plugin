0a28df4b574261a8c10f2bd6df69326de3a9c0b3
NetherMC <thelio.trinite@outlook.com>
2021-11-26 18:38:58 +0100

    website

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <GitHub />
+  <Content />
+</template>
+
+<script setup lang="ts">
+import GitHub from './components/GitHub.vue';
+import Content from './components/Content.vue';
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <section id="presentation" class="text-center">
+      <h1>Social Commands</h1>
+
+      <div style="height: 0.75rem;"></div>
+
+      <p>A Spigot Plugin that adds commands like <code>/discord</code> to your server</p>
+
+      <div style="height: 2rem;"></div>
+
+      <div class="buttons">
+        <a href="https://www.spigotmc.org/resources/socialcommands.95783/" class="button">Download</a>
+        <a href="https://nethermc.gitbook.io/socialcommands/" class="button">Wiki</a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 4rem;
+}
+
+#presentation {
+  h1 {
+    font-size: 2.1rem;
+  }
+
+  p {
+    font-size: 1.3rem;
+  }
+
+  .buttons {
+    .button {
+      font-size: 1.2rem;
+      text-transform: uppercase;
+      font-weight: bolder;
+      letter-spacing: 1px;
+      display: inline-block;
+
+      &:not(:first-of-type) {
+        margin-left: 1rem;
+      }
+    }
+  }
+}
+</style>
